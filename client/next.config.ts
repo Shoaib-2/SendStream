cc59b9a2b40e342daab43bdef57e845fcc2642a8
Mohammed Shoaib <shoaibkkhn50665@gmail.com>
2025-06-02 23:20:31 +0100

    Frontend deployment now

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,11 @@ const nextConfig = {
   async redirects() {
     return [];
   },
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+  }
 }
 
 module.exports = nextConfig;
