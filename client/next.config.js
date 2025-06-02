744b57e1b352ba3f4670c4ebb8242ab0cf7428ed
Mohammed Shoaib <shoaibkkhn50665@gmail.com>
2025-06-02 23:40:11 +0100

    next.config is a  JS file now

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,13 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [];
-  },
-  output: 'standalone',
+  swcMinify: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_STRIPE_PRICE_ID: process.env.NEXT_PUBLIC_STRIPE_PRICE_ID
   }
-}
+};
 
 module.exports = nextConfig;
